650ffa28d6b42a0ba62fa7d15a8392679651e7ab
admiral44 <shubham.nagane44@gmail.com>
2022-06-01 11:30:08 +0530

    manifest file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import Resolver from '@forge/resolver';
 
 const resolver = new Resolver();
 
+const issuePanelResolver = new Resolver();
+export const handlerIssuePannel = issuePanelResolver.getDefinitions();
+
+const adminPageResolver = new Resolver();
+export const handlerAdminPage = adminPageResolver.getDefinitions();
+
 resolver.define('getText', (req) => {
     console.log(req);
-
     return 'Hello, world!';
 });
 
